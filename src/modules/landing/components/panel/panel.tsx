371f151ffdd120834123dbf86c6e6a5f39648019
Zoltan Kertesz <kertesz.zoltan.9718@gmail.com>
2024-06-26 23:18:31 +0200

    landing page panel and background video setup

---
+++ src/modules/landing/components/panel/panel.tsx
@@ -0,0 +1,12 @@
+import "./panel.scss";
+import BackgroundVideo from "../backgroundVideo/backgroundVideo";
+
+export default function Landing() {
+  return (
+    <section className="panel">
+      <div className="panel-content">
+        <BackgroundVideo />
+      </div>
+    </section>
+  );
+}
