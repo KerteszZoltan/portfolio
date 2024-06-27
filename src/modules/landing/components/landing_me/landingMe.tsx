4a016346dfc2ee028d737819d8f3486599e744f4
Zoltan Kertesz <kertesz.zoltan.9718@gmail.com>
2024-06-27 14:55:09 +0200

    landing page with introduction text

---
+++ src/modules/landing/components/landing_me/landingMe.tsx
@@ -0,0 +1,22 @@
+import "./landingMe.scss";
+
+export default function () {
+  return (
+    <div className="landingMe-container-background">
+      <div className="landingMe-container-content">
+        <h1 className="landingMe-container-content h1">
+          Hi! My name is Zoltán Kertész
+        </h1>
+        <p>
+          I live in Hungary! I'm a back-end PHP developer who now meet with
+          front-end frameworks.
+        </p>
+        <p>My main goal is to be fullstack developer.</p>
+
+        <p>etc etc..</p>
+      </div>
+    </div>
+  );
+}
+
+/**/
--- src/modules/landing/components/panel/panel.tsx
+++ src/modules/landing/components/panel/panel.tsx
@@ -1,11 +1,16 @@
 import "./panel.scss";
 import BackgroundVideo from "../backgroundVideo/backgroundVideo";
+import landingMe from "../landing_me/landingMe";
+import LandingMe from "../landing_me/landingMe";
 
 export default function Landing() {
   return (
     <section className="panel">
       <div className="panel-content">
         <BackgroundVideo />
+        <div className="panel-content-text">
+          <LandingMe />
+        </div>
       </div>
     </section>
   );
