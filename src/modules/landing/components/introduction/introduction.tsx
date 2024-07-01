5cfa36a561bd8c80eb8ad255fc28d2c1f9cd60db
Zoltan Kertesz <kertesz.zoltan.9718@gmail.com>
2024-07-01 21:46:21 +0200

    primary button visual effect changed

--- src/modules/landing/components/introduction/introduction.tsx
+++ src/modules/landing/components/introduction/introduction.tsx
@@ -1,11 +1,11 @@
 import PrimaryButton from "@/modules/common/components/buttons/primaryButton";
 import "./introduction.scss";
+import "@/modules/common/styles/global.scss";
 
 export default function Introduction() {
   return (
     <div className="intoduction-container">
       <h3 className="secoundary-title">Zoltán Kertész</h3>
-      <span className="spacer"></span>
       <h1 className="primary-title">Full-stack developer</h1>
       <span className="spacer"></span>
 
