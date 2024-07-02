d99711d118a135b7b635f9b3d3ff367c4bdcc272
Zoltan Kertesz <kertesz.zoltan.9718@gmail.com>
2024-07-02 15:16:24 +0200

    Navigation button and style frame

--- src/modules/common/components/navigation/navigation.tsx
+++ src/modules/common/components/navigation/navigation.tsx
@@ -2,64 +2,67 @@
 
 import "./navigation.scss";
 import "../../styles/global.scss";
-import NavButton from "../buttons/navigationButton";
+import PrimaryButton from "../buttons/primaryButton";
+import { faDownload, faPlay } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navigation() {
   return (
     <nav className="navigation-container">
-      <div className="page-counter">
+      <div className="page-counter-container">
         <div className="page-counter-item">{"<"}</div>
         <div className="page-counter-item">{">"}</div>
       </div>
-      <div className="item">
-        <img
-          className="background-image"
-          src="/assets/images/Linkedin-logo.png"
-          alt="LinkedIn logo"
-        ></img>
+      <div className="item-container">
+        <img src="/assets/images/Linkedin-logo.png" alt="LinkedIn logo"></img>
         <p className="primary-title">LinkedIn</p>
-        <p className="nav-description">My profile</p>
+        <p className="nav-title">My profile</p>
         <span className="spacer"></span>
-        <NavButton
-          onClick={function (): void {
-            console.log("clicked");
-          }}
-          text={"linkedIn"}
-        />
-      </div>
-      <div className="item">
-        <img
-          className="background-image"
-          src="/assets/images/github-logo.png"
-          alt="GitHub logo"
-        ></img>
-        <p className="primary-title">GitHub</p>
-        <p className="nav-description">My repositories</p>
-        <span className="spacer"></span>
-        <div>
-          <NavButton
+        <div className="button-container">
+          <PrimaryButton
             onClick={function (): void {
-              console.log("clicked");
+              throw new Error("Function not implemented.");
             }}
-            text={"Github"}
+            text={"LinkedIn"}
+            icon={faPlay}
           />
         </div>
       </div>
-      <div className="item">
+      <div className="item-container">
+        <img src="/assets/images/github-logo.png" alt="GitHub logo"></img>
+        <p className="primary-title">GitHub</p>
+        <p className="nav-title">Check my repository</p>
+        <p className="nav-description">
+          {" "}
+          Here can you check my portfolio and previous projects
+        </p>
+        <span className="spacer"></span>
+        <div className="button-container">
+          <PrimaryButton
+            onClick={function (): void {
+              throw new Error("Function not implemented.");
+            }}
+            text={"GitHub"}
+            icon={faPlay}
+          />
+        </div>
+      </div>
+      <div className="item-container">
         <img
-          className="background-image"
           src="/assets/images/contactus-logo.png"
           alt="Contact us logo"
         ></img>
-        <p className="primary-title">Contact us</p>
-        <p className="nav-description"> My personal informations</p>
+        <p className="primary-title">CV</p>
+        <p className="nav-title"> Download my CV</p>
         <span className="spacer"></span>
-        <NavButton
-          onClick={function (): void {
-            console.log("clicked");
-          }}
-          text={"contact"}
-        />
+        <div className="button-container">
+          <PrimaryButton
+            onClick={function (): void {
+              throw new Error("Function not implemented.");
+            }}
+            text={"Download"}
+            icon={faDownload}
+          />
+        </div>
       </div>
     </nav>
   );
